5ad50d7605a483a82b6109cb668db0c776b171a3
joao <joao.nilton@gmail.com>
2022-03-29 17:25:33 -0300

    ver_1_29032022

--- Aula012/operadoreslogicos.c
+++ Aula012/operadoreslogicos.c
@@ -74,3 +74,4 @@ printf("Resultado = %d \n", r);
 //Estou aqui em 24 de março de 2022. Vamos continuar?
 //Estou aqui em 25 de março de 2022. Vamos continuar?
 //Estou aqui em 28 de março de 2022. Vamos continuar?
+//Estou aqui em 29 de março de 2022. Vamos continuar?
