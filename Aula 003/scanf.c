105b4082c54e2d08931a37520938ad52d54d9b49
joao <joao.nilton@gmail.com>
2022-04-08 16:59:03 -0300

    ver_1_08042022

--- Aula 003/scanf.c
+++ Aula 003/scanf.c
@@ -65,3 +65,4 @@ return 0;
 //Estou aqui em 05 de abril de 2022. Vamos continuar?
 //Estou aqui em 06 de abril de 2022. Vamos continuar?
 //Estou aqui em 07 de abril de 2022. Vamos continuar?
+//Estou aqui em 08 de abril de 2022. Vamos continuar?
